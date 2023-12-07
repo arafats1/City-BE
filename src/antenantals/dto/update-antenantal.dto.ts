815195d0b8b5cc359ec83a32ded2d8a092f98608
arafats1 <arafats144@gmail.com>
2023-12-07 10:43:40 +0300

    initial

--- src/antenantals/dto/update-antenantal.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAntenantalDto } from './create-antenantal.dto';
-
-export class UpdateAntenantalDto extends PartialType(CreateAntenantalDto) {}
--- src/vaccinations/dto/update-vaccination.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateVaccinationDto } from './create-vaccination.dto';
-
-export class UpdateVaccinationDto extends PartialType(CreateVaccinationDto) {}
